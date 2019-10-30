0c6a30b67dad075356ba0c75a433fa63773598a6
cowkeys <cowkeys@outlook.com>
2019-10-30 11:42:51 +0000

    finish get api

--- management/src/main.rs
+++ management/src/main.rs
@@ -9,9 +9,11 @@ extern crate serde;
 use serde::{Serialize, Deserialize};
 use rocket::response::status;
 use rocket::response::{self,Responder,Response};
+use std::io::Cursor;
 use rocket::http::{Status, ContentType};
 use rocket::request::Request;
 use rocket_contrib::json::Json;
+use serde_json::json;
 
 
 #[derive(Serialize)]
@@ -22,7 +24,14 @@ struct User {
     updated : i64,
 }
 
-
+impl<'r> Responder<'r> for User {
+    fn respond_to(self, _: &Request) -> response::Result<'r> {
+        Response::build()
+            .sized_body(Cursor::new(json!(self).to_string()))
+            .header(ContentType::new("application", "json"))
+            .ok()
+}
+}
 
 #[post("/user/<id>")]
 fn addUser(id: usize) -> status::Accepted<String> {
@@ -31,14 +40,13 @@ fn addUser(id: usize) -> status::Accepted<String> {
 
 //fn getUser(id: usize) -> Json<User> {
 #[get("/user/<id>")]
-fn getUser(id: usize) -> Option<User> {
-
-    Option<User{
+fn getUser(id: usize) -> User {
+    User{
         id:id,
         name:"cowkeys".to_string(),
         age:19,
         updated:0
-    }>
+    }
     //Json(u)
 }
 
